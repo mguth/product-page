cb5f7fb78e9f0e95177b4c33c27c7778f5715d74
Moritz Guth <moritz.guth@gmail.com>
2019-04-10 09:42:11 +0200

    update config data

--- config/website.js
+++ config/website.js
@@ -3,11 +3,11 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
+  siteTitle: 'Murmel 3000- Gatsby Starter Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Murmel 3000', // Alternative Site title for SEO
+  siteTitleShort: 'Murmel3000', // short_name for manifest
   siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://murmel-3000.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
